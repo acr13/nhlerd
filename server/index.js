624654d0f57108121afe25e2cf7ab536edab9e60
Alex Richardson <alex@rangle.io>
2015-09-16 20:43:31 -0400

    added cors

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ const port = process.env.PORT || 8000;
 
 require('./routes')(app);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
