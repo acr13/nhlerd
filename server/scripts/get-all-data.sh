7962396323944b8e5ac387cdc1abe90c4611844b
Alex Richardson <alex@rangle.io>
2016-03-26 15:12:00 -0400

    Revert

--- server/scripts/get-all-data.sh
+++ server/scripts/get-all-data.sh
@@ -3,6 +3,6 @@
 # Run all of out data-scrapers
 # babel-node --stage 0 -- server/scripts/get-goalie-stats.js erroring out with even str
 
-babel-node --stage 0 -- server/scripts/get-player-stats.js
-babel-node --stage 0 -- server/scripts/get-player-enh-stats.js
-babel-node --stage 0 -- server/scripts/get-player-spc-stats.js
+./node_modules/.bin/babel-node -- server/scripts/get-player-stats.js
+./node_modules/.bin/babel-node-- server/scripts/get-player-enh-stats.js
+./node_modules/.bin/babel-node -- server/scripts/get-player-spc-stats.js
