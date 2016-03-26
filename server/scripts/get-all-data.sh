8dee5bfac0aa9310e99d3bb2915f04f8ee2e4e05
Alex Richardson <alex@rangle.io>
2016-03-26 15:15:37 -0400

    asdad

--- server/scripts/get-all-data.sh
+++ server/scripts/get-all-data.sh
@@ -4,5 +4,5 @@
 # babel-node --stage 0 -- server/scripts/get-goalie-stats.js erroring out with even str
 
 ./node_modules/.bin/babel-node -- server/scripts/get-player-stats.js
-./node_modules/.bin/babel-node-- server/scripts/get-player-enh-stats.js
+./node_modules/.bin/babel-node -- server/scripts/get-player-enh-stats.js
 ./node_modules/.bin/babel-node -- server/scripts/get-player-spc-stats.js
