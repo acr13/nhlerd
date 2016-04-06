d014a83ac76a9018a6b3b518a7508c4f4721c47f
Alex Richardson <alex@rangle.io>
2016-04-06 15:57:26 -0400

    gamessssss

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -4,79 +4,89 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 const shortForms = require('../utils/shortForms');
 const R = require('ramda');
+const moment = require('moment');
 
-const BASE_GAMES_URL = 'http://www.nhl.com/ice/scores.htm';
-const BASE_GAME_EXTENDED_URL = 'http://www.nhl.com/gamecenter/en/preview?id=';
+const BASE_GAMES_URL = 'https://statsapi.web.nhl.com/api/v1/schedule';
 
 let find = function (req, res) {
-  rp(BASE_GAMES_URL)
-    .then(getGamesFromHTML)
-    .then(function (games) {
-      res.send(games);
+  const today = moment().format('YYYY-MM-DD');
+
+  return rp(BASE_GAMES_URL + '?startDate=' + today + '&endDate=' + today)
+    .then(pluckGamesFromResponse)
+    .then(function (response) {
+      res.send(response);
     });
 };
 
 let findByGameId = function (req, res) {
-  var gameId = req.params.gameId;
+  const gameId = parseInt(req.params.gameId);
+  const today = moment().format('YYYY-MM-DD');
 
-  rp(BASE_GAME_EXTENDED_URL + gameId)
-    .then(function (resp) {
-      res.send('abc');
-    })
+  return rp(BASE_GAMES_URL + '?startDate=' + today + '&endDate=' + today)
+    .then(pluckGamesFromResponse)
+    .then(function (response) {
+      const game = response.games.filter((game) => {
+        return gameId === game.gamePk;
+      });
+
+      return res.send(game);
+    });
 };
 
 let findByTeamId = function (req, res) {
-  var teamName = shortForms[req.params.teamId];
+  const team = req.params.teamId;
+  const today = moment().format('YYYY-MM-DD');
 
-  if (!teamName) {
-    res.json({
-      success: false,
-      error: 'Invalid team name type'
+  return rp(BASE_GAMES_URL + '?startDate=' + today + '&endDate=' + today)
+    .then(pluckGamesFromResponse)
+    .then(function (response) {
+      const game = response.games.filter((game) => {
+        return team === game.teams.away.team.name ||
+          team === game.teams.home.team.name;
+      });
+
+      return res.send(game);
     });
-    return;
+};
+
+
+let findByDate = function (req, res) {
+  const date = req.query.date;
+
+  return rp(BASE_GAMES_URL + '?startDate=' + date + '&endDate=' + date)
+    .then(pluckGamesFromResponse)
+    .then(function (response) {
+      res.send(response);
+    });
+};
+
+let findByDateRange = function (req, res) {
+  const start = req.query.startDate;
+  const end = req.query.endDate;
+
+  return rp(BASE_GAMES_URL + '?startDate=' + start + '&endDate=' + end)
+    .then(function (response) {
+      response = JSON.parse(response);
+      res.send(response.dates);
+    });
+};
+
+let pluckGamesFromResponse = function (response) {
+  response = JSON.parse(response);
+
+  if (response.dates.length > 0) {
+    return R.head(response.dates);
   }
 
-  function filterGameByTeamName (game) {
-    return game.homeTeam === teamName
-      || game.awayTeam === teamName;
-  }
-
-  rp(BASE_GAMES_URL)
-    .then(getGamesFromHTML)
-    .then(R.filter(filterGameByTeamName))
-    .then(function (games) {
-      res.send(games);
-    });
-}
-
-let getGamesFromHTML = function (html) {
-  var $ = cheerio.load(html);
-  var games = [];
-
-  $(".gamebox").each(function (idx, ele) {
-    var game = {
-      gameId: $(this).find(".gcLinks a").attr("href").substr(-10),
-      time: $(this).find("table th.left").text().replace(/<!--[\s\S]*?-->/g, ""),
-      homeTeam: shortForms[$(this).find("tr.homeTeam td.team a").html()],
-      homeScore: $(this).find("tr.homeTeam td.total").html(),
-      awayTeam: shortForms[$(this).find("table td.team a").html()],
-      awayScore: $(this).find("tr:not(.homeTeam) td.total").html(),
-    };
-
-    if (game.time.indexOf("FINAL") !== -1) {
-      game.live = false;
-    } else if (game.time.indexOf("ET") !== -1) {
-      game.live = false;
-    } else {
-      game.live = true;
-    }
-
-    games.push(game);
-  });
-
-  return games;
+  return {
+    date: 'invalid date',
+    games: [],
+    totalItems: 0
+  };
 };
 
 exports.find = find;
 exports.findByGameId = findByGameId;
 exports.findByTeamId = findByTeamId;
+exports.findByDate = findByDate;
+exports.findByDateRange = findByDateRange;
--- server/routes/gameRouter.js
+++ server/routes/gameRouter.js
@@ -4,7 +4,9 @@ const router = require('express').Router();
 const controller = require('../controllers/gameController');
 
 router.get('/', controller.find);
-router.get('/:gameId', controller.findByGameId);
+router.get('/id/:gameId', controller.findByGameId);
 router.get('/team/:teamId', controller.findByTeamId);
+router.get('/date', controller.findByDate);
+router.get('/daterange', controller.findByDateRange);
 
 module.exports = router;
